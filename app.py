da24f95aaf0ede41a9e7697dd7d7f5ac064b3cab
soarbear <soarbear7@gmail.com>
2018-11-24 19:28:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
+"""
+    Web Order Tool for ZAIF.
+    Copyright (c) 2018 http://soarcloud.com.  All rights reserved.
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details at:
+    http://www.gnu.org/licenses/gpl.html
+"""
 import os, sys, time
 from flask import Flask, request, render_template
 sys.path.append('/usr/local/lib/python3.6/site-packages/zaif_client')
